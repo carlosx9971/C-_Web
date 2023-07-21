bf72a19ca0a7c985c23aee0e88833f5b41d699e0
ccaraballo <ccaraballo@camsoft.com.do>
2023-07-21 17:26:11 -0400

    arreglos y listas

---
+++ Arreglos y listas/Arreglos y listas/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos_y_listas
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        public string[] Identificaciones { get; set; }
+    }
+}
